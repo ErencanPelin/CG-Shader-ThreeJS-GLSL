121d23f0af7ebdba90e7f07cead723ace02d29c2
Erencan Pelin <erencan.pelin@gmail.com>
2023-05-31 10:40:12 +1000

    setup shader base

--- Fire_shader/build/shaders/fire_vertex.glsl
+++ Fire_shader/build/shaders/fire_vertex.glsl
@@ -1,12 +1,3 @@
-//FRAGMENT
-varying vec3 vNormal;
-varying vec2 vTextureCoord;
-
-void main()
-{
-    gl_FragColor = vec4( texCoordV, 0.0, 1.0 );
-}
-
 //VERTEX
 varying vec3 vNormal;
 varying vec2 vTextureCoord;
