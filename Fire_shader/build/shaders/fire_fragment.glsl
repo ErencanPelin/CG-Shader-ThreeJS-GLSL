2a8e663535a3e277f5a386e14c65e90ef81f49cf
Erencan Pelin <erencan.pelin@gmail.com>
2023-05-31 20:59:34 +1000

    add flame gui

--- Fire_shader/build/shaders/fire_fragment.glsl
+++ Fire_shader/build/shaders/fire_fragment.glsl
@@ -1,30 +1,68 @@
 uniform float u_time;
-    varying vec3 vNormal;
-    varying vec3 vPosition;
-    varying vec2 vTextureCoord;
+uniform float u_speed;
+uniform float u_flameThinningFactor = 3.0;
+uniform float u_flameEdgeWidth = 0.07;
+uniform float u_waveStrength; //max 0.1
+uniform float u_waveFrequency; //max 30
 
-    void main()
+varying vec3 vNormal;
+varying vec3 vPosition;
+varying vec2 vTextureCoord;
+
+const float pi = 3.14;
+const vec2 center = vec2(0.5, 0.5);
+
+float circularVignette(in vec2 screenUV, in vec2 offset, in float scale) {
+    return 1.0 - (length(screenUV - offset - center) / 0.5) / scale;
+}
+
+float circularVignette(in vec2 screenUV) { 
+      return circularVignette(screenUV, vec2(sin((u_time * u_speed) + (screenUV.y * -u_waveFrequency)) * u_waveStrength, 0.0), 1.0); 
+}
+
+void main() {
+    vec2 st = clamp(vPosition.xy, 0.0, 1.0);// gl_FragCoord.xy/u_resolution.xy;
+
+    float fade;
+    fade = clamp(fade + (1.0 - st.y) * 0.8, 0.0, 1.0);
+
+    if (abs(st.x - 0.5) > 0.5 / flameThinningFactor)
     {
-		//	gl_FragColor = vec4(vPosition, 1.0 );
-
-    vec2 st = vPosition.xy;
-    //  st.x *= u_resolution.x/u_resolution.y;
-  
-      vec3 firePos = vec3(0, 0.2, 0);
-      float strength = 10.0;
-      float height = -0.1;
-      float range = 3.816;
-      float speed = 5.0;
-      float curveSteps = 2.0;
-      float flameSteps = 15.0;
-      float curveAmount = 0.05;
-      
-      float dist = distance(firePos, 
-                            vec3(st.x + (sin(st.y * curveSteps + (u_time * speed)) * curveAmount * st.y), 
-                                 st.y * height, 
-                                 0.0));
-      dist = floor((1.0 - clamp(pow(dist * range, strength), 0.0, 1.0)) * flameSteps) / flameSteps;
-      vec3 color = vec3(dist, dist * 0.5, dist * 0.2);
-  
-      gl_FragColor = vec4(color,1.0);
+        gl_FragColor = vec4(0.0);
+        return;
     }
+    else
+    {
+    fade *= cos((st.x - 0.5) * pi * flameThinningFactor);
+    }
+    float blue = fade;
+
+    if (st.y < 0.5)
+    {
+    fade *= circularVignette(st);   
+    }
+    else
+    {
+    fade *= 1.0 - abs(st.x - center.x) / 0.5;
+    }
+    float red = fade;
+    float clampVal = st.y;
+    if (fade < clampVal)
+    {
+    gl_FragColor = vec4(0.0);
+    return;
+    }
+    else if (fade < clampVal + flameEdgeWidth)
+    {
+    fade = (fade - clampVal) / flameEdgeWidth;
+    }
+    else
+    {
+    fade = 1.0;
+    }
+
+    vec3 flameCol = mix(vec3(0.024,0.383,1.000), vec3(0.036,0.343,0.900), blue);
+    vec3 outerCol = mix(vec3(0.985,0.727,0.086), vec3(7.0,2.2,2.0), red);
+    vec3 col = mix(flameCol, outerCol, fade);
+    gl_FragColor = vec4(col * fade, 1.0);
+}
--- Fire_shader/build/shaders/fire_vertex.glsl
+++ Fire_shader/build/shaders/fire_vertex.glsl
@@ -1,9 +1,12 @@
-//VERTEX
 varying vec3 vNormal;
+varying vec3 vPosition;
 varying vec2 vTextureCoord;
+
 void main()
 {
     vNormal = (normalMatrix * normal);
     vec4 ViewPosition= modelViewMatrix * vec4(position,1.0);
+    vPosition = ViewPosition.xyz;
+    vPosition.x += 0.5;
     gl_Position=projectionMatrix * ViewPosition;
 }
